59f6207113803d3ffcc5325c381e28518756912d
Augusto Lopez Brenot <augustolopezbrenot@gmail.com>
2023-03-07 10:40:24 -0300

    Unión de templates entre apps

--- AppPosts/urls.py
+++ AppPosts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from AppPosts import views
 
 urlpatterns = [
-    
+    path('prueba/', views.prueba, name='prueba')
 ]
--- AppPosts/views.py
+++ AppPosts/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 # clase 19 | diapo 38  y  clase 22 | diapo 40 para hacer la view list de posts, tambien falta una para crear los posts
 # ver como hacer para que los templates de las apps puedan redirigirse entre sí
+
+def prueba(request):
+    return render(request, 'AppPosts/inicio.html')
